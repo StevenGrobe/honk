15086402d3a208345704fd9c3efced10fd94deb0
StevenGrobe <steven.grobe@le-campus-numerique.fr>
2021-05-06 11:21:21 +0200

    verification form et crytapage password

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,11 +4,13 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id
@@ -29,9 +31,14 @@ class User
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Length(min="8", minMessage="8 characters")
+     * @Assert\EqualTo(propertyPath="confirm_password", message="wrong password")
      */
     private $password;
 
+    /**
+     * @Assert\EqualTo(propertyPath="password", message="wrong password")
+     */
     public $confirm_password;
 
     public function getId(): ?int
@@ -73,5 +80,18 @@ class User
         $this->password = $password;
 
         return $this;
+    }
+
+    public function eraseCredentials()
+    {
+    }
+
+    public function getSalt()
+    {
+    }
+
+    public function getRoles()
+    {
+        return ['ROLE_USER'];
     }
 }
