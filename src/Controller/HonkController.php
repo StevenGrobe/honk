1a8c5d919a2bdcdb64bb27f275ba84fa3d410efb
StevenGrobe <steven.grobe@le-campus-numerique.fr>
2021-05-06 13:22:54 +0200

    possible de créé des honks

--- src/Controller/HonkController.php
+++ src/Controller/HonkController.php
@@ -8,6 +8,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+
 class HonkController extends AbstractController
 {
     /**
@@ -34,10 +37,19 @@ class HonkController extends AbstractController
      * @Route("/create", name="create")
      */
 
-    public function create()
+    public function create(Request $request, EntityManagerInterface $manager)
     {
         $honk = new Honk();
         $form = $this->createForm(HonkType::class, $honk);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $honk->setHonks($this->getUser());
+            $honk->setCreatedAt(new \DateTime());
+            $manager->persist($honk);
+            $manager->flush();
+
+            return $this->redirectToRoute('list');
+        }
         return $this->render('honk/create.html.twig', [
             'form' => $form->createView()
         ]);
